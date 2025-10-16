3f702d299e7ad142f68e50fc909fb2810809907f
xperimentalone <sklaisuki@gmail.com>
2025-10-16 17:17:31 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANT: Replace '<your-repo-name>' with the name of your GitHub repository.
-  // For example, if your repository URL is https://github.com/user/my-app,
-  // the base should be '/my-app/'.
-  base: '/<your-repo-name>/',
+  // Set the base path for GitHub Pages deployment.
+  base: '/insight-quest/',
 })
